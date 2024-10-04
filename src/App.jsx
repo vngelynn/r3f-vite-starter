e4d32552e63884384d1f7846b379f193b7250dcd
Angelynn <89512890+vngelynn@users.noreply.github.com>
2024-10-03 23:10:29 -0700

    add sections and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Canvas } from "@react-three/fiber"
 import { Experience } from "./components/Experience"
+import { Scroll, ScrollControls } from "@react-three/drei"
+import { NavLinks } from "./components/NavLinks.jsx"
+import { ScrollManager } from "./components/ScrollManager.jsx"
+import { useState } from "react"
 
 function App() {
+  const [section, setSection] = useState(0)
+
   return (
     <>
       <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
         <color attach='background' args={["#ececec"]} />
-        <Experience />
+        <ScrollControls pages={4} damping={0.1}>
+          <ScrollManager section={section} onSectionChange={setSection} />
+          <Experience />
+          <Scroll html>
+            <NavLinks />
+          </Scroll>
+        </ScrollControls>
       </Canvas>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </>
   )
 }
